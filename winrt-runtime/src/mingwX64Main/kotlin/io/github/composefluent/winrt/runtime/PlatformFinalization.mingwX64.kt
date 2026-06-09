54b8f58afee29c58b903f91986551d4cc566836e
sanlorng <fujia625@gmail.com>
2026-06-10 02:57:58 +0800

    Drain native finalizers on mingw runtime cleanup

--- winrt-runtime/src/mingwX64Main/kotlin/io/github/composefluent/winrt/runtime/PlatformFinalization.mingwX64.kt
+++ winrt-runtime/src/mingwX64Main/kotlin/io/github/composefluent/winrt/runtime/PlatformFinalization.mingwX64.kt
@@ -1,5 +1,11 @@
+@file:OptIn(kotlin.native.runtime.NativeRuntimeApi::class)
+
 package io.github.composefluent.winrt.runtime
 
+import kotlin.native.runtime.GC
+
 internal actual object PlatformFinalization {
-    actual fun drain() = Unit
+    actual fun drain() {
+        GC.collect()
+    }
 }
