fb0d3b15f0bccbf1e40092cda0950e49aba00a54
sanlorng <fujia625@gmail.com>
2026-06-26 11:11:25 +0800

    Use generated projection support registration

--- winrt-runtime/src/mingwX64Main/kotlin/io/github/composefluent/winrt/runtime/GeneratedMetadataInitialization.mingwX64.kt
+++
@@ -1,7 +0,0 @@
-package io.github.composefluent.winrt.runtime
-
-import kotlin.reflect.KClass
-
-internal actual fun platformTryInitializeGeneratedWinRtMetadata(type: KClass<*>) {
-    // Native projection support is initialized through compiler-generated support registrars.
-}
