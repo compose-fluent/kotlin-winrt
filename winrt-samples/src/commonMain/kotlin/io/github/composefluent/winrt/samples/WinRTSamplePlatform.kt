c516756ab15cdb1da5caa6bf0ba745dc17e29fdb
sanlorng <fujia625@gmail.com>
2026-07-09 00:08:29 +0800

    Move WinUI sample smoke sources to winuiMain

--- winrt-samples/src/commonMain/kotlin/io/github/composefluent/winrt/samples/WinRTSamplePlatform.kt
+++ winrt-samples/src/commonMain/kotlin/io/github/composefluent/winrt/samples/WinRTSamplePlatform.kt
@@ -3,3 +3,5 @@ package io.github.composefluent.winrt.samples
 internal expect fun winRTSampleOption(name: String): Boolean
 
 internal expect fun winRTSampleOptionConfigured(name: String): Boolean
+
+internal expect fun runWinUiSample()
